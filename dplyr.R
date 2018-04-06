c721324e5a882a002a10a6c48bfaf374fad17a70
Sehrish Kanwal <kanwals@unimelb.edu.au>
2018-04-06 10:57:38 +1000

    some cool stuff

--- dplyr.R
+++ dplyr.R
@@ -15,12 +15,41 @@ msleep %>% select(name, sleep_total) %>% mutate(sleep_total_minute = sleep_total
 
 #Create two new coloumns 
 msleep %>% select(name, sleep_total) %>% 
-           mutate(sleep_total_vs_avg = sleep_total - round(mean(sleep_total), 1),
-           sleep_total_vs_min = sleep_total - min(sleep_total))
+  mutate(sleep_total_vs_avg = sleep_total - round(mean(sleep_total), 1),
+  sleep_total_vs_min = sleep_total - min(sleep_total))
 
 #Create column using if else and arrange
 msleep %>% select(name, brainwt) %>% 
-           mutate(brainwt2 = ifelse(brainwt > 4, NA, brainwt)) %>% 
-           arrange(desc(brainwt))
+  mutate(brainwt2 = ifelse(brainwt > 4, NA, brainwt)) %>% 
+  arrange(desc(brainwt))
+
+msleep %>% select(name) %>% 
+  mutate(name_last_word = tolower(str_extract(name, pattern = "\\w+$")))
+
+#Screw the data
+msleep_ohno <- msleep %>% 
+  mutate_all(~paste(., "  /n  "))
+
+#Let's clean it now
+msleep_corr <- msleep_ohno %>% 
+  mutate_all(~str_replace_all(., "/n", "")) %>% 
+  mutate_all(str_trim)
+
+#Applying condition to mutate
+msleep %>% select(name, sleep_total:bodywt) %>% 
+  mutate_if(is.numeric, round)
+
+#Applying mutate to specific coloumns names
+
+msleep %>% select(name, sleep_total:awake) %>% 
+  mutate_at(vars(contains("sleep")), ~(.*60))
+
+
+
+
+
+
+
+
 
 
